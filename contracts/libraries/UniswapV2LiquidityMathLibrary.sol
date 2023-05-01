23f14671820f6053628ff82e99837978e152cd18
~ . ~ <81006672+lilesper@users.noreply.github.com>
2023-04-30 22:15:16 -0400

    a lil build

--- contracts/libraries/UniswapV2LiquidityMathLibrary.sol
+++ contracts/libraries/UniswapV2LiquidityMathLibrary.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
-import '@uniswap/lib/contracts/libraries/FullMath.sol';
+import '@mageswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@mageswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@mageswap/lib/contracts/libraries/Babylonian.sol';
+import '@mageswap/lib/contracts/libraries/FullMath.sol';
 
 import './SafeMath.sol';
 import './UniswapV2Library.sol';
